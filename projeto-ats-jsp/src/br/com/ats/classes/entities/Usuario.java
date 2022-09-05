21ce30819cc439ccae881568c29ac0cd0fb668a7
Leandro Amorim <leandroamorimpb@gmail.com>
2022-09-05 10:27:07 -0300

    Criando outros atributos da classe Usuário

--- projeto-ats-jsp/src/br/com/ats/classes/entities/Usuario.java
+++ projeto-ats-jsp/src/br/com/ats/classes/entities/Usuario.java
@@ -1,7 +1,74 @@
 package br.com.ats.classes.entities;
 
-public class Usuario {
+import java.sql.Timestamp;
 
+public class Usuario extends GenericDomain {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nome;
+	private String cpf;
+	private String email;
 	private String login;
 	private String senha;
+	private Timestamp dataCadastro;
+	
+	public Usuario() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Timestamp getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Timestamp dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [nome=" + nome + ", cpf=" + cpf + ", email=" + email + ", login=" + login + ", senha=" + senha
+				+ ", dataCadastro=" + dataCadastro + "]";
+	}
+
 }
